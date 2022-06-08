2b7e73d91ffa311a6dfe384f32e794a0ab8e5d11
victorsalomao <victor.salomaor@gmail.com>
2022-06-08 18:43:44 -0300

    atividades 15, 16 e ainda nao iniciei a 17

--- atividade15/script.js
+++ atividade15/script.js
@@ -1,10 +1,12 @@
 let input = require("fs").readFileSync("stdin", "utf8");
 let valores = input.split("\n");
 
-let x1 = valores.shift().split(" ");
-let x2 = valores.shift().split(" ");
-let y1 = valores.shift().split(" ");
-let y2 = valores.shift().split(" ");
+let [x1, y1]= valores[0].split(" ");
+let [x2, y2]= valores[1].split(" ");
 
+let calculo1 = Math.pow(x2 - x1, 2);
+let calculo2 = Math.pow(y2 - y1, 2);
 
-console.log(x1, x2, y1, y2);
+let distancia = Math.sqrt(calculo1 + calculo2);
+
+console.log(distancia.toFixed(4));
